640c7f8c30d3938af8e29df81fe73b54f2193bf6
Fernandoggs <fernando.ggs@gmail.com>
2016-10-10 13:43:00 +0200

    Ejercicio 3 terminado

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -10,7 +10,7 @@ class SmallSMILHandler(ContentHandler):
         
         self.width = ""
         self.height = ""
-        self.background-color = ""
+        self.backgroundcolor = ""
         self.id = ""
         self.top = ""
         self.bottom = ""
@@ -25,23 +25,56 @@ class SmallSMILHandler(ContentHandler):
         self.audiodur = ""
         self.textstreamsrc = ""
         self.textstreamregion = ""
+        self.lista = []
         
     def startElement(self, name, attrs):
     
-        if name == 'root-layout'
+        if name == 'root-layout':
             self.width = attrs.get('width',"")
             self.height = attrs.get('height',"")
-            self.background-color = attrs.get('background-color',"")
-        elif name == 'region'
+            self.backgroundcolor = attrs.get('backgroundcolor',"")
+            atributes = {'width': self.width,
+                         'height': self.height,
+                         'backgroundcolor': self.backgroundcolor}
+            atributes['tag'] = name
+            self.lista.append(atributes)
+        elif name == 'region':
             self.id = attrs.get('id',"")
             self.top = attrs.get('top',"")
             self.bottom = attrs.get('bottom',"")
             self.left = attrs.get('left',"")
             self.right = attrs.get('right',"")
-        elif name == 'img'
+            atributes = {'id': self.id,
+                         'top': self.top,
+                         'bottom':self.bottom,
+                         'left': self.left,
+                         'right': self.right}
+            atributes['tag'] = name
+            self.lista.append(atributes)
+
+        elif name == 'img':
             self.imgsrc = attrs.get('img',"")
             self.imgregion = attrs.get('region',"")
-            self.imgbegin =
-            
+            self.imgbegin = attrs.get('begin',"")
+            self.imgdur = attrs.get('dur',"")
+        elif name == 'audio':
+        	self.audiosrc = attrs.get('src',"")
+        	self.audiobegin = attrs.get('begin',"")
+        	self.audiodur = attrs.get('dur',"")
+        elif name == 'textstream':
+        	self.textstreamsrc = attrs.get('src',"")
+        	self.textstreamregion = attrs.get('region',"")
+
+    def get_tags(self):
+    		return self.lista
+
+if __name__ == "__main__":
+
+    parser = make_parser()
+    sHandler = SmallSMILHandler()
+    parser.setContentHandler(sHandler)
+    parser.parse(open('karaoke.smil'))
+    Data = sHandler.get_tags()
+    print(Data)        
             
 
